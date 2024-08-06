f8956e83e68338daa9a0dff1e7415e0a588bcdf8
WaqasSQA <jjonsnoww1291@gmail.com>
2024-08-06 18:26:39 +0500

    first-commit

---
+++ Mocha.js
@@ -0,0 +1,5 @@
+describe('App Feature', function(){
+    it('should print Hello World', function(){
+        console.log("Hello World")
+    })
+})
---
+++ test-config.js
@@ -0,0 +1,18 @@
+exports.config = {
+    runner: 'local',
+    port: '4723',
+    host: 'localhost',
+    path: '/wd/hub',
+    logLevel: 'info',
+    framework: 'mocha',
+    mochaOpts: {
+        ui: 'bdd',
+        require: ['@babel/register'],
+        timeout: 600000
+    },
+    maxInstances: 1,
+    sync: true,
+    specs: [
+        './Mocha.js'
+    ]
+}
